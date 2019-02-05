8672a67b33dd1670f84e373941067fd77452bdf3
Robert Beall <rbeall@quotient-inc.com>
2019-02-05 17:23:07 -0500

    Added query vars validation and documentation

--- libs/utilities.php
+++ libs/utilities.php
@@ -1,4 +1,14 @@
 <?php
+  /**
+   * Simple functions for testing, debugging, and other simple,
+   * modularized tasks
+   */
+
+  /**
+   * function for logging php strings to browser console for testing and
+   * debugging
+   * @param  String $content content to be logged
+   */
   function console_log($content)
   {
     echo '<script>console.log("'.$content.'")</script>';
