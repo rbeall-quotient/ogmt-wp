3bdcc3d2140edca716f30e2a55384871501302a8
Robert Beall <rbeall@quotient-inc.com>
2019-02-25 11:33:35 -0500

    Added top level menu and sub-menu. Modified link

--- libs/admin/ogmt-admin-menu.php
+++ libs/admin/ogmt-admin-menu.php
@@ -9,7 +9,9 @@
    */
   function ogmt_add_menu()
   {
-      add_submenu_page('options-general.php', 'OGMT', 'OGMT Settings', 'manage_options', 'ogmt-settings', 'ogmt_admin_menu');
+      //add_submenu_page('options-general.php', 'OGMT', 'OGMT Settings', 'manage_options', 'ogmt-settings', 'ogmt_admin_menu');
+      add_menu_page('OGMT', 'OGMT Settings', 'manage_options', 'ogmt-settings', 'ogmt_admin_menu');
+      add_submenu_page('ogmt-settings', 'OGMT Basic Settings', 'Basic Settings', 'manage_options', 'ogmt-basics', 'ogmt_basics_menu');
   }
 
   /**
@@ -25,6 +27,18 @@
   }
 
   /**
+   * Echo ogmt html to admin plugin settings page.
+   */
+  function ogmt_basics_menu()
+  {
+    ?>
+    	<div class="wrap">
+    		<h2>OGMT Basics</h2>
+    	</div>
+    <?php
+  }
+
+  /**
    * Add a settings link on the plugin page for OGMT linking to settings page
    *
    * @param array $links array of action links with new link appended.
@@ -33,7 +47,7 @@
   function add_action_link( $links )
   {
     $settings_link = array(
-      '<a href="' . admin_url( 'options-general.php?page=ogmt-settings') . '">' . __('Settings', 'ogmt-settings') . '</a>',
+      '<a href="' . admin_url( 'admin.php?page=ogmt-settings') . '">' . __('Settings', 'ogmt-settings') . '</a>',
     );
 
     return array_merge( $links, $settings_link);
